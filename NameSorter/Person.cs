b05cdb61ef675a0ef825858e73559fee151fc362
Matt <Matt@DESKTOP-05HVCIT>
2018-10-08 19:27:16 +0100

    added missing files

---
+++ NameSorter/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NameSorter
+{
+    public class Person
+    {
+        public string lastName { get; set; }
+        public string givenNames { get; set; }
+        //private static List<Person> people = new List<Person>();
+
+        public Person(string lastName, string givenNames)
+        {
+            this.lastName = lastName;
+            this.givenNames = givenNames;
+
+            
+        }
+    }
+}
